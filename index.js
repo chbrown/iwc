aff41860cbbc3afc2a3d81fa4d57341e7f515911
Christopher Brown <io@henrian.com>
2018-03-03 02:17:31 -0600

    Delint

--- index.js
+++ index.js
@@ -1,14 +1,11 @@
 #!/usr/bin/env node
-'use strict'; /*jslint node: true, es5: true, indent: 2 */
-var fs = require('fs');
-
 var started = Date.now();
 
-var args = (function(argv) {
+function parseArgs(argv) {
   var args = {}, f = 0;
   for (var arg, i = 0; (arg = argv[i]); i++) {
     if (arg.match(/^-.+/)) {
-      var next = argv[i+1];
+      var next = argv[i + 1];
       if (arg.match(/=/)) {
         var parts = arg.split(/=/);
         arg = parts[0];
@@ -27,13 +24,16 @@ var args = (function(argv) {
     }
   }
   return args;
-})(process.argv.slice(2)); // slice off the first two arguments, [node, iwc, ...]
+}
+var args = parseArgs(process.argv.slice(2)); // slice off the first two arguments, [node, iwc, ...]
 
 var show_l = args.l; // lines
 var show_w = args.w; // words
 var show_c = args.c; // characters
 if (!show_l && !show_w && !show_c) {
-  show_l = show_w = show_c = true;
+  show_l = true;
+  show_w = true;
+  show_c = true;
 }
 
 var count_l = 0;
@@ -42,12 +42,15 @@ var count_c = 0;
 
 function printCounts() {
   var counts = [];
-  if (show_l)
+  if (show_l) {
     counts.push(count_l);
-  if (show_w)
+  }
+  if (show_w) {
     counts.push(count_w);
-  if (show_c)
+  }
+  if (show_c) {
     counts.push(count_c);
+  }
   process.stdout.write('\r' + counts.join('\t') + ' ', 'utf8');
 }
 
@@ -56,12 +59,15 @@ var precision = 4;
 function printRates() {
   var elapsed_seconds = (Date.now() - started) / 1000;
   var rates = [];
-  if (show_l)
+  if (show_l) {
     rates.push((count_l / elapsed_seconds).toFixed(precision));
-  if (show_w)
+  }
+  if (show_w) {
     rates.push((count_w / elapsed_seconds).toFixed(precision));
-  if (show_c)
+  }
+  if (show_c) {
     rates.push((count_c / elapsed_seconds).toFixed(precision));
+  }
   process.stdout.write('\r' + rates.join('\t') + ' ', 'utf8');
 }
 
@@ -70,13 +76,15 @@ var printLine = args.r ? printRates : printCounts; // rate
 process.stdin.on('data', function(chunk) {
   if (show_l) {
     var newlines = chunk.match(/\n/g);
-    if (newlines)
+    if (newlines) {
       count_l += newlines.length;
+    }
   }
   if (show_w) {
     var words = chunk.match(/\S+/g);
-    if (words)
+    if (words) {
       count_w += words.length;
+    }
   }
   if (show_c) {
     count_c += chunk.length;
